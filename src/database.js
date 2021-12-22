0e7e06c3f617395a422331f4970c4782c82011ba
Eklavya <76553593+eklavyadev@users.noreply.github.com>
2021-12-22 21:54:03 +0530

    database.js 🚀
    For creating database and connection of PostgreSQL

--- src/database.js
+++ src/database.js
@@ -5,11 +5,11 @@ const dbConfig = parseDbUrl(process.env.DATABASE_URL);
 
 const client = new Client({
 	user: dbConfig.user,
-    host: dbConfig.host,
-    database: dbConfig.database,
-    password: dbConfig.password,
-    port: dbConfig.port,
-    ssl: { rejectUnauthorized: false }
+    	host: dbConfig.host,
+    	database: dbConfig.database,
+    	password: dbConfig.password,
+    	port: dbConfig.port,
+    	ssl: { rejectUnauthorized: false }
 })
 
 client.on("connect", ()=>{
@@ -20,17 +20,9 @@ client.on("end", ()=>{
 	console.log("Database Disconnected!")
 })
 
+// Creating Database
 client.query(
-	'CREATE TABLE IF NOT EXISTS Post (sno INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY, title VARCHAR, slug VARCHAR, content VARCHAR, date TEXT);', (err, res)=>{
-	if(!err){
-		console.log(res);
-	}else{
-		console.log(err)
-	}
-})
-
-client.query(
-	"CREATE TABLE IF NOT EXISTS Project (sno INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY, title VARCHAR, content VARCHAR, link VARCHAR, git VARCHAR, category VARCHAR,date TEXT)", (err, res)=>{
+	'CREATE TABLE IF NOT EXISTS Post (sno INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY, title VARCHAR, slug VARCHAR, content VARCHAR, date Date);', (err, res)=>{
 	if(!err){
 		console.log(res);
 	}else{
